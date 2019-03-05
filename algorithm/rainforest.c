faa61f11d9622c6028285c01c1bf65263e1e07e1
Aiwe <maksymivv@gmail.com>
2019-03-05 19:15:22 +0200

    Revert "Bring back mbc version attempt"
    This reverts commit f136830cb515c5fa4a22cebdc61d88aff9ce779e.

--- algorithm/rainforest.c
+++ algorithm/rainforest.c
@@ -722,18 +722,20 @@ static void rf256_init(rf256_ctx_t *ctx) {
 
 // update the hash context _ctx_ with _len_ bytes from message _msg_
 static void rf256_update(rf256_ctx_t *ctx, const char *msg, size_t len) {
+  const uint8_t *msg8 = (uint8_t *)msg;
+
   while (len > 0) {
 #ifdef RF_UNALIGNED_LE32
     if (!(ctx->len&3) && len>=4) {
-      ctx->word=*(uint32_t *)msg;
+      ctx->word=*(uint32_t *)msg8;
       ctx->len+=4;
       rf256_one_round(ctx);
-      msg+=4;
+      msg8+=4;
       len-=4;
       continue;
     }
 #endif
-    ctx->word|=((uint32_t)*(uint8_t *)msg++)<<(8*(ctx->len++&3));
+    ctx->word|=((uint32_t)*msg++)<<(8*(ctx->len++&3));
     len--;
     if (!(ctx->len&3))
       rf256_one_round(ctx);
