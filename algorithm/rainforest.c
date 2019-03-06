d0432e17735fa2690407a9180eb399e30c18749b
Aiwe <maksymivv@gmail.com>
2019-03-06 19:37:22 +0200

    Fixing rainforest windows build

--- algorithm/rainforest.c
+++ algorithm/rainforest.c
@@ -179,16 +179,16 @@ static void aes2r_encrypt(uint8_t * state, uint8_t * key) {
 
 #else
     /* first round of the algorithm */
-    add_round_key(state, (void*)&key_schedule[0]);
+    add_round_key(state, (uint8_t*)&key_schedule[0]);
     sub_bytes(state);
     shift_rows(state);
     mix_columns(state);
-    add_round_key(state, (void*)&key_schedule[4]);
+    add_round_key(state, (uint8_t*)&key_schedule[4]);
 
     /* final round of the algorithm */
     sub_bytes(state);
     shift_rows(state);
-    add_round_key(state, (void*)&key_schedule[8]);
+    add_round_key(state, (uint8_t*)&key_schedule[8]);
 
 #endif
 }
@@ -505,7 +505,7 @@ static inline uint32_t rf_rambox(uint64_t *rambox, uint64_t old) {
 }
 
 // write (_x_,_y_) at cell _cell_ for offset _ofs_
-static inline void rf_w128(uint64_t *cell, ulong ofs, uint64_t x, uint64_t y) {
+static inline void rf_w128(uint64_t *cell, size_t ofs, uint64_t x, uint64_t y) {
 #if defined(__ARM_ARCH_8A) || defined(__AARCH64EL__)
   // 128 bit at once is faster when exactly two parallelizable instructions are
   // used between two calls to keep the pipe full.
@@ -721,19 +721,21 @@ static void rf256_init(rf256_ctx_t *ctx) {
 }
 
 // update the hash context _ctx_ with _len_ bytes from message _msg_
-static void rf256_update(rf256_ctx_t *ctx, const void *msg, size_t len) {
+static void rf256_update(rf256_ctx_t *ctx, const char *msg, size_t len) {
+  const uint8_t *msg8 = (uint8_t *)msg;
+
   while (len > 0) {
 #ifdef RF_UNALIGNED_LE32
     if (!(ctx->len&3) && len>=4) {
-      ctx->word=*(uint32_t *)msg;
+      ctx->word=*(uint32_t *)msg8;
       ctx->len+=4;
       rf256_one_round(ctx);
-      msg+=4;
+      msg8+=4;
       len-=4;
       continue;
     }
 #endif
-    ctx->word|=((uint32_t)*(uint8_t *)msg++)<<(8*(ctx->len++&3));
+	ctx->word |= ((uint32_t)*msg8++) << (8 * (ctx->len++ & 3));
     len--;
     if (!(ctx->len&3))
       rf256_one_round(ctx);
@@ -765,7 +767,7 @@ static void rf256_final(void *out, rf256_ctx_t *ctx) {
 void rf256_hash(void *out, const void *in, size_t len) {
   rf256_ctx_t ctx;
   rf256_init(&ctx);
-  rf256_update(&ctx, in, len);
+  rf256_update(&ctx, (char *)in, len);
   rf256_final(out, &ctx);
 }
 
@@ -774,22 +776,9 @@ void rainforest_precompute(const void *in, void *out)
   rf256_ctx_t ctx;
 
   rf256_init(&ctx);
-  rf256_update(&ctx, in, 76);
+  rf256_update(&ctx, (char *)in, 76);
   memcpy(out, &ctx, sizeof(ctx));
   //fprintf(stderr, "rf_precompute : cached %d bytes at %p\n", (int)sizeof(ctx), out);
-}
-
-/*
- * Encode a length len/4 vector of (uint32_t) into a length len vector of
- * (unsigned char) in big-endian form.  Assumes len is a multiple of 4.
- */
-static inline void
-be32enc_vect(uint32_t *dst, const uint32_t *src, uint32_t len)
-{
-	uint32_t i;
-
-	for (i = 0; i < len; i++)
-		dst[i] = htobe32(src[i]);
 }
 
 void rainforest_regenhash(struct work *work)
